ecdfcfd7fbd1e489f4825a13088b2ba5f28c940c
Rakhshan khan <rakshaankhan1@gmail.com>
2024-01-22 16:40:46 +0530

    Move the javascript Code to script.js file

--- script.js
+++ script.js
@@ -1,66 +1,17 @@
-let keyInputString = ''
-let percentageClicked = 0;
-let parenthesis = 0
-
-// Part to show the input and output on the screen
-
-function handleShowInputInUpperPart(input) {
-  document.getElementById('upper').innerText = input
-  
+function keyInput(value) {
+  document.getElementById('screen').value += value
 }
-function handleShowInputLowerPart(result) {
-  document.getElementById('middle').innerText = result
+function backspace() {
+  let value = document.getElementById('screen').value.slice(0, -1)
+  document.getElementById('screen').value = value
 }
 
-// handle the key input
-function handleKeyInput(key) {
-        keyInputString += key.innerText
-        handleShowInputInUpperPart(keyInputString)
-    
+function clr() {
+  document.getElementById('screen').value = ''
 }
 
-// handle the clear key
-function handleTheClearKey() {
-  keyInputString = ''
-  handleShowInputInUpperPart('')
-  handleShowInputLowerPart('')
-}
-
-// handle Parenthesis
-function handleParenthesis() {
-  if (parenthesis == 0) {
-    keyInputString += '('
-    parenthesis = 1
-    handleShowInputInUpperPart(keyInputString)
-  } else {
-    keyInputString += ')'
-    parenthesis = 0
-    handleShowInputInUpperPart(keyInputString)
-  }
-}
-
-// handle parensentage key
-function handlePercentageOperator(){
-    keyInputString += '/100'
-     handleShowInputInUpperPart(keyInputString)
-     const result = eval(keyInputString)
-     handleShowInputLowerPart(result)
-}
-
-// handle cut functionality
-function handleCutButton() {
-  keyInputString = keyInputString.slice(0, -1)
-  handleShowInputInUpperPart(keyInputString)
-}
-
-// handleOutput app
-function handleOutput(output) {
-  try {
-    const result = eval(keyInputString)
-    handleShowInputLowerPart(result)
-    return
-  } catch (error) {
-    handleShowInputLowerPart('Invalid input')
-    return 'Error'
-  }
+function solve() {
+  let value = document.getElementById('screen').value
+  let result = eval(value)
+  document.getElementById('screen').value = result
 }
